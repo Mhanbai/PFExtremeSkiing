61af7f8e09d4b807e6cb10d1ed1180150cb10ad8
Gregor Manby <gregormanby@gmail.com>
2019-02-18 23:30:10 +0000

    Project files

---
+++ Assets/Scripts/ScrollingBackgroundAsset.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackgroundAsset : MonoBehaviour {
+	//Editor variables
+	[SerializeField] private float speed;
+
+	//Reference variables
+	private SpriteRenderer myRenderer;
+
+	void Start() {
+		myRenderer = GetComponent<SpriteRenderer> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 offset = new Vector3 (Time.time * speed, 0.0f, 0.0f);
+		myRenderer.material.mainTextureOffset = offset;
+	}
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+	//Editor variables
+	[SerializeField] private GameObject toSpawn;
+	[SerializeField] private float minSpawnTime = 0.1f;
+	[SerializeField] private float maxSpawnTime = 0.6f;
+
+	//Working variables
+	private float timeToNextSpawn;
+	private float timeAtLastSpawn;
+
+	// Use this for initialization   
+	void Start () {
+		timeAtLastSpawn = Time.time;
+		timeToNextSpawn = Random.Range (minSpawnTime, maxSpawnTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time - timeAtLastSpawn > timeToNextSpawn) {
+			GameObject.Instantiate (toSpawn, transform.position, Quaternion.identity);
+			timeToNextSpawn = Random.Range (minSpawnTime, maxSpawnTime);
+			timeAtLastSpawn = Time.time;
+		}
+	}
+}
